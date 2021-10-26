fafd6316bed19013df35f77f9f926447ca73bd5d
anujpant08 <anujpant2@gmail.com>
2021-10-26 19:44:14 +0530

    Added share functionality for sharing list as text across apps.

--- app/src/main/java/com/example/grocerylist/ListDetailViewActivity.java
+++ app/src/main/java/com/example/grocerylist/ListDetailViewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,6 +16,9 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -67,11 +71,31 @@ public class ListDetailViewActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        ImageView shareImageView = findViewById(R.id.share_button_detail);
+        shareImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateAndSaveGroceryItem();
+                String jsonContent = gson.toJson(groceryItem);
+                Log.e(TAG, "final jsoncontent to share: " + jsonContent);
+                jsonContent = formatContent(jsonContent);
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, jsonContent);
+                sendIntent.setType("text/plain");
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+            }
+        });
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        updateAndSaveGroceryItem();
+    }
+
+    private void updateAndSaveGroceryItem() {
         groceryItem = finalListParentAdapter.getGroceryItem();
         Log.e(TAG, "List from final parent adapter: " + groceryItem);
         SharedPreferences sharedPreferencesGroceryList = this.getSharedPreferences(SAVEDLIST, Context.MODE_PRIVATE);
@@ -95,5 +119,64 @@ public class ListDetailViewActivity extends AppCompatActivity {
             editor.putString(SAVEDLIST, jsonContent);
             editor.apply();
         }
+    }
+    private String formatContent(String jsonContent){
+        StringBuilder formattedResult = new StringBuilder(groceryItem.getTitle() + "\r\n");
+        try{
+            JSONObject jsonObject = new JSONObject(jsonContent);
+            JSONArray fruitArray = jsonObject.optJSONArray("fruitList");
+            JSONArray vegetableArray = jsonObject.optJSONArray("vegetableList");
+            JSONArray spiceArray = jsonObject.optJSONArray("spiceList");
+            JSONArray otherArray = jsonObject.optJSONArray("othersList");
+            if(fruitArray != null && fruitArray.length() > 0){
+                formattedResult.append("Fruits:").append("\r\n");
+                for(int index = 0; index < fruitArray.length(); index++){
+                    JSONObject fruit = (JSONObject) fruitArray.get(index);
+                    formattedResult.append(fruit.get("name")).append("-");
+                    if(!fruit.get("quantity").equals("")){
+                        formattedResult.append(fruit.get("quantity")).append("\r\n");
+                    }else if(!fruit.get("weight").equals("")){
+                        formattedResult.append(fruit.get("weight")).append("\r\n");
+                    }
+                }
+            }if(vegetableArray != null && vegetableArray.length() > 0){
+                formattedResult.append("Vegetables:").append("\r\n");
+                for(int index = 0; index < vegetableArray.length(); index++){
+                    JSONObject vegetable = (JSONObject) vegetableArray.get(index);
+                    formattedResult.append(vegetable.get("name")).append("-");
+                    if(!vegetable.get("quantity").equals("")){
+                        formattedResult.append(vegetable.get("quantity")).append("\r\n");
+                    }else if(!vegetable.get("weight").equals("")){
+                        formattedResult.append(vegetable.get("weight")).append("\r\n");
+                    }
+                }
+            }if(spiceArray != null && spiceArray.length() > 0){
+                formattedResult.append("Spices:").append("\r\n");
+                for(int index = 0; index < spiceArray.length(); index++){
+                    JSONObject spice = (JSONObject) spiceArray.get(index);
+                    formattedResult.append(spice.get("name")).append("-");
+                    if(!spice.get("quantity").equals("")){
+                        formattedResult.append(spice.get("quantity")).append("\r\n");
+                    }else if(!spice.get("weight").equals("")){
+                        formattedResult.append(spice.get("weight")).append("\r\n");
+                    }
+                }
+            }if(otherArray != null && otherArray.length() > 0){
+                formattedResult.append("Others:").append("\r\n");
+                for(int index = 0; index < otherArray.length(); index++){
+                    JSONObject other = (JSONObject) otherArray.get(index);
+                    formattedResult.append(other.get("name")).append("-");
+                    if(!other.get("quantity").equals("")){
+                        formattedResult.append(other.get("quantity")).append("\r\n");
+                    }else if(!other.get("weight").equals("")){
+                        formattedResult.append(other.get("weight")).append("\r\n");
+                    }
+                }
+            }
+            Log.e(TAG, "formattedrresult: " + formattedResult.toString());
+        }catch(Exception e){
+            Log.e(TAG, "An exception has occurred: ", e);
+        }
+        return formattedResult.toString();
     }
 }
